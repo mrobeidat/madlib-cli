2fd4ebb62d422abd6289a7ff014297fb97288236
Yousef Obeidat <y.linux96@gmail.com>
2021-11-02 16:18:09 +0200

    finsish

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,22 +1,57 @@
 
+import re
 
-def read_template(path: str) -> str:
 
-    if path != "assets/text1.txt":
+# def read_template(path: str) -> str:
 
-        return FileNotFoundError('no path found')
+#     if path != "assets/text1.txt":
 
-    with open(path, 'r') as file:
+#         return FileNotFoundError('no path found')
+
+#     with open(path, 'r') as file:
+
+#         file_content = file.read()
+#         words = re.findall('\{.*?\}', file_content)
+
+#         print(words)
+#         for i in range(len(words)):
+#             name = input("Enter an "[i]+"!")
+#             print(name)
+#     return file_content
+
+def read_template(filepath: str) -> str:
+
+    if filepath != "assets/text1.txt":
+
+        raise FileNotFoundError('no path found')
+
+    with open(filepath, 'r') as file:
 
         file_content = file.read()
 
-    return file_content
+    return file_content.strip()
+
+# read_template('assets/text1.txt')
 
 
-def merge(string,word):
-    statment=string.format(*word)
-    
-    with open('assets/dark_and_stormy_night_template.txt','w') as f:
-     f.write(statment)
-     
-    return statment
+def parse_template(file_content: str):
+
+    parse = re.findall(r'\{(.*?)\}', file_content)
+
+    for item in parse:
+
+        file_content = file_content.replace((item), '', 2)
+
+    return file_content, tuple(parse)
+
+
+
+def merge(file_content, parse):
+
+    updatedText = file_content.format(*parse)
+
+    with open('assets/text3.txt', 'w') as result:
+
+        result.write(updatedText)
+
+    return updatedText
